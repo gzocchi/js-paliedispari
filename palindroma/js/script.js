5cc073f789ce620e921919a2860fc8a00aa45e2c
gzocchi <z.giorgio.z@gmail.com>
2021-05-03 14:09:43 +0200

    modificato script palindoma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,22 +1,23 @@
 // 1 - Palindroma
 // Chiedere all'utente di inserire una parola
 
-var parola = prompt("Inserisci una parola")
+var parola = prompt("Inserisci una parola").trim().toLowerCase();
 
 // Creare una funzione per capire se la parola inserita è palindroma
 
 function palindroma (str) {
   // scompongo la string in un array di sottostringhe
-  var stringaReverse = str.split(''); 
-  console.log(stringaReverse);
   // Riordino gli elementi dell'array in ordine inverso
-  stringaReverse = stringaReverse.reverse(); 
-  console.log(stringaReverse);
   // Unisco nuovamente tutti gli elementi di un array in una singola stringa
-  stringaReverse = stringaReverse.join('');
-  console.log(stringaReverse);
-  // Valore booleano per uguaglianza stringhe minuscole
-  return str.toLowerCase() == stringaReverse.toLowerCase();
+  var stringaReverse = str.split('').reverse().join('');
+  // Valore booleano per uguaglianza stringhe (== true != false)
+  return str == stringaReverse;
+};
+
+function frasePalindroma (strPhrase) {
+    var fraseReverse = strPhrase.replace(/ /g,'').split('').reverse().join('');
+    return strPhrase.replace(/ /g,'') == fraseReverse;
 };
 
 console.log(palindroma(parola));
+console.log(frasePalindroma("i topi non avevano nipoti"));
